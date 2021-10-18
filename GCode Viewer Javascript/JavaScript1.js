bc94297d92dba3e80bda8cb247b4f0404a07a454
SEAN-DESKTOP\Sean <sean.desktop@gmail.com>
2021-10-18 06:15:25 -0500

    gcode viewer javascript

---
+++ GCode Viewer Javascript/JavaScript1.js
@@ -0,0 +1,2 @@
+﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.12.748.0/runtimes/native1.12-tchmi/TcHmi.d.ts" />
