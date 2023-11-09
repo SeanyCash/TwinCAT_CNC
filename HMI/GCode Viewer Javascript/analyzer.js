8e07a24f4c491aeba2c6b2493a836db54428d747
Sean-Peterson <sean.desktop@gmail.com>
2023-11-09 11:10:29 -0600

    Reworking production project to be compatible with TwinCAT BSD

--- HMI/GCode Viewer Javascript/analyzer.js
+++ HMI/GCode Viewer Javascript/analyzer.js
@@ -1,4 +1,4 @@
-﻿// Keep these lines for a best effort IntelliSense of Visual Studio 2017.
+// Keep these lines for a best effort IntelliSense of Visual Studio 2017.
 /// <reference path="./../Packages/Beckhoff.TwinCAT.HMI.Framework.12.748.0/runtimes/native1.12-tchmi/TcHmi.d.ts" />
 
 /**
